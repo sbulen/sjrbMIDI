88783cbc21b873f16e7ff8fb3222675487107d0c
sbulen <bulens@pacbell.net>
2024-06-08 13:11:07 -0700

    Add setters for sequence properties

--- sources/class-AbstractSequence.php
+++ sources/class-AbstractSequence.php
@@ -143,5 +143,90 @@ abstract class AbstractSequence
 	{
 		return $this->trip_pct;
 	}
+
+	/**
+	 * Set rhythm...
+	 *
+	 * @param Rhythm $rhythm
+	 * @return void
+	 */
+	public function setRhythm(Rhythm $rhythm): void
+	{
+		if (is_a($rhythm, 'Rhythm'))
+			$this->rhythm = $rhythm;
+		else
+			Errors::fatal('inv_rhythm');
+	}
+
+	/**
+	 * Set downbeat...
+	 *
+	 * @param int $downbeat
+	 * @return void
+	 */
+	public function setDownbeat(int $downbeat = 1): void
+	{
+		if (is_int($downbeat) && $downbeat > 0)
+			$this->downbeat = $downbeat;
+		else
+			Errors::fatal('inv_downbeat');
+	}
+
+	/**
+	 * Set duration...
+	 *
+	 * @param int $duration
+	 * @return void
+	 */
+	public function setDuration(int $duration = 1): void
+	{
+		if (is_int($duration) && $duration > 0)
+			$this->duration = $duration;
+		else
+			Errors::fatal('inv_dur');
+	}
+
+	/**
+	 * Set destinations...
+	 *
+	 * @param int[] $destinations
+	 * @return void
+	 */
+	public function setDestinations(array $destinations = array(1)): void
+	{
+		// Make sure destinations are all ints
+		if (is_array($destinations) && (count($destinations) > 0) && ($destinations == array_filter($destinations, function($a) {return is_int($a);})))
+			$this->destinations = $destinations;
+		else
+			Errors::fatal('inv_dests');
+	}
+
+	/**
+	 * Set note_pct...
+	 *
+	 * @param float $note_pct
+	 * @return void
+	 */
+	public function setNotePct(float $note_pct = 1): void
+	{
+		if (is_numeric($note_pct) && ($note_pct >= 0) && ($note_pct <= 1))
+			$this->note_pct = $note_pct;
+		else
+			Errors::fatal('inv_notepct');
+	}
+
+	/**
+	 * Set trip_pct...
+	 *
+	 * @param float $trip_pct
+	 * @return void
+	 */
+	public function setTripPct(float $trip_pct = 0): void
+	{
+		if (is_numeric($trip_pct) && ($trip_pct >= 0) && ($trip_pct <= 1))
+			$this->trip_pct = $trip_pct;
+		else
+			Errors::fatal('inv_trippct');
+	}
 }
 ?>
